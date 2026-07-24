2f3202a778594c04955f63786e5a223f979fc607
SDET Engineer <yny2019@gmail.com>
2026-07-24 15:25:20 +0900

    Update blog name to Test Insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://observer-blog.vercel.app',
+	site: 'https://test-insights.vercel.app',
 });
